053dee6ea50a2c8d54280d7b924333e4ecc0d12d
Jeremy Black <jeremyblack@Jeremys-MacBook-Air.local>
2022-01-25 14:24:07 -0500

    Removed unnecessary files

--- app/src/main/java/com/example/lonelymountainnav/view/Fragment.ext.kt
+++
@@ -1,8 +0,0 @@
-package com.example.lonelymountainnav.view
-
-import android.widget.Toast
-import androidx.fragment.app.Fragment
-
-fun Fragment.toast(name: String) {
-    Toast.makeText(requireContext(), "My name is $name", Toast.LENGTH_LONG).show()
-}
--- app/src/main/java/com/example/lonelymountainnav/view/View.ext.kt
+++
@@ -1,7 +0,0 @@
-package com.example.lonelymountainnav.view
-
-import com.google.android.material.textfield.TextInputLayout
-
-fun TextInputLayout.convertToString(): String {
-    return this.editText?.text.toString() ?: "No input"
-}
